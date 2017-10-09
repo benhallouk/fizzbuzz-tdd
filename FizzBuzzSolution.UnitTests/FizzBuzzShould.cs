57d1d27ec23fd0fbc1522692422416e7822053fc
Amine Benhallouk <benhallouk>
2017-10-09 17:05:40 +0100

    add initial code

---
+++ FizzBuzzSolution.UnitTests/FizzBuzzShould.cs
@@ -0,0 +1,44 @@
+﻿namespace FizzBuzzSolution.UnitTests
+{
+    using NUnit.Framework;
+    using System.Linq;
+
+    [TestFixture]
+    public class FizzBuzzShould
+    {
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "Fizz")]
+        [TestCase(6, "Fizz")]
+        [TestCase(9, "Fizz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(30, "FizzBuzz")]
+        [TestCase(60, "FizzBuzz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(10, "Buzz")]
+        public void ReturnAFizzBuzzValue(int inputNumber, string expectedResult)
+        {
+            Assert.AreEqual(expectedResult, FizzBuzzConverter.Convert(inputNumber));
+        }
+
+        [Test]
+        public void CanGet100Answers()
+        {
+            Assert.AreEqual(100, FizzBuzzConverter.GetAnswers().Count());
+        }
+
+        [Test]
+        public void ReturnAnswersWhereFirstItemIs1()
+        {
+            Assert.AreEqual("1", FizzBuzzConverter.GetAnswers().First());
+        }
+
+        [Test]
+        public void ReturnsTheCorrectAnswers()
+        {
+            var correctAnswers = Enumerable.Range(1, 100).Select(FizzBuzzConverter.Convert);
+
+            Assert.AreEqual(correctAnswers, FizzBuzzConverter.GetAnswers());
+        }
+    }
+}
---
+++ FizzBuzzSolution/FizzBuzzConverter.cs
@@ -0,0 +1,31 @@
+﻿namespace FizzBuzzSolution
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class FizzBuzzConverter
+    {
+        public static string Convert(int i)
+        {
+            if ((i % 3 + i % 5) == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (i % 3 == 0)
+            {
+                return "Fizz";
+            }
+            if (i % 5 == 0)
+            {
+                return "Buzz";
+            }
+
+            return i.ToString();
+        }
+
+        public static IEnumerable<string> GetAnswers()
+        {
+            return new string[100].Select(x => "1");
+        }
+    }
+}
